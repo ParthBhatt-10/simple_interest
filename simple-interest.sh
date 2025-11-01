4e7530817b4d334390aad50f72fc03fc8d141961
PARTH BHATT <139350837+ParthBhatt-10@users.noreply.github.com>
2025-11-01 14:29:08 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Simple Interest Calculation
+
+echo "Enter Principal:"
+read p
+echo "Enter Rate:"
+read r
+echo "Enter Time:"
+read t
+
+si=$(( (p * r * t) / 100 ))
+
+echo "Simple Interest = $si"
